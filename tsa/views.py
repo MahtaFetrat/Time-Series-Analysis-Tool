924f69c9f65d5d53c170a9a4791500c33ad0168b
MahtaFetrat <77fetrat@gmail.com>
2022-12-06 01:50:00 +0330

    implement minimal csv upload and read

--- tsa/views.py
+++ tsa/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic.edit import FormView
+from tsa.forms import UploadFileForm
+import pandas as pd
 
 # Create your views here.
+
+class IndexView(FormView):
+    template_name = 'tsa/index.html'
+    form_class = UploadFileForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        df = pd.read_csv(self.request.FILES['file'])
+        return super().form_valid(form)
