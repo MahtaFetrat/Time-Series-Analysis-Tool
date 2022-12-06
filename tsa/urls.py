0a6fd7a649469a46fb8731cd46a204e0a432eb2b
MahtaFetrat <77fetrat@gmail.com>
2022-12-07 01:36:19 +0330

    implement basic data passing and preview in the visualization view

--- tsa/urls.py
+++ tsa/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from tsa.views import IndexView
+from tsa.views import IndexView, VisualizationView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('visualization/', VisualizationView.as_view(), name='visualization'),
 ]
--- tsa/views.py
+++ tsa/views.py
@@ -1,4 +1,5 @@
 from django.views.generic.edit import FormView
+from django.views.generic.base import TemplateView
 from tsa.forms import UploadFileForm
 import pandas as pd
 
@@ -7,8 +8,14 @@ import pandas as pd
 class IndexView(FormView):
     template_name = 'tsa/index.html'
     form_class = UploadFileForm
-    success_url = '/'
+    success_url = 'visualization/'
 
     def form_valid(self, form):
         df = pd.read_csv(self.request.FILES['file'])
+        self.request.session['df'] = df.iloc[:, 0].tolist()
         return super().form_valid(form)
+
+
+class VisualizationView(TemplateView):
+    template_name = "tsa/visualization.html"
+    
