f91fe2a8a3cfa0bc451d448cb70af950bd0d082a
MahtaFetrat <77fetrat@gmail.com>
2022-12-08 12:09:27 +0330

    add data summary

--- tsa/views.py
+++ tsa/views.py
@@ -12,10 +12,32 @@ class IndexView(FormView):
 
     def form_valid(self, form):
         df = pd.read_csv(self.request.FILES['file'])
-        self.request.session['df'] = df.iloc[:, 0].tolist()
+        self.request.session['data'] = df.iloc[:, 0].tolist()
         return super().form_valid(form)
 
 
 class VisualizationView(TemplateView):
+    PREVIEW_COUNT = 5
     template_name = "tsa/visualization.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        self.set_context_datapoints(context)
+        self.set_context_summary(context)
+        self.set_context_plot(context)
+        
+        return context
+
+    def set_context_datapoints(self, context):
+        data = self.request.session.get('data')
+
+        context['start_indices'] = range(1, self.PREVIEW_COUNT + 1)
+        context['start_data'] = data[:self.PREVIEW_COUNT]
+        context['end_indices'] = range(len(data) - self.PREVIEW_COUNT, len(data))
+        context['end_data'] = data[-self.PREVIEW_COUNT:]
     
+    def set_context_summary(self, context):
+        data = pd.Series(self.request.session.get('data'))
+        summary = data.describe().to_dict()
+        summary = {k: round(v, 2) for k, v in summary.items()}
+        context['summary'] = summary
